1eb5dbd154e316660fd44a4d3f4efd3eecd6cdc4
ErikBoesen <me@erikboesen.com>
2016-09-16 08:11:58 -0400

    Add countdown clock. (Needs to use the updated NT value)

--- vufine.js
+++ vufine.js
@@ -8,7 +8,8 @@ var ui = {
 		arm: document.getElementById('gyro-arm'),
 		number: document.getElementById('gyro-number')
 	},
-    mask: document.getElementById('mask')
+    mask: document.getElementById('mask'),
+    timer: document.getElementById('timer')
 };
 
 // Sets function to be called when any NetworkTables key/value changes
@@ -48,5 +49,41 @@ function onValueChanged(key, value, isNew) {
                     ui.mask.style.backgroundColor = 'transparent';
             }
             break;
+        case '/SmartDashboard/startTheTimer':
+			// When this NetworkTables variable is true, the timer will start.
+			// You shouldn't need to touch this code, but it's documented anyway in case you do.
+			var s = 135;
+			if (value) {
+				// Make sure timer is reset to black when it starts
+				ui.timer.style.color = 'white';
+				// Function below adjusts time left every second
+				var countdown = setInterval(function() {
+					s--; // Subtract one second
+					// Minutes (m) is equal to the total seconds divided by sixty with the decimal removed.
+					var m = Math.floor(s / 60);
+					// Create seconds number that will actually be displayed after minutes are subtracted
+					var visualS = (s % 60);
+
+					// Add leading zero if seconds is one digit long, for proper time formatting.
+					visualS = visualS < 10 ? '0' + visualS : visualS;
+
+					if (s < 0) {
+						// Stop countdown when timer reaches zero
+						clearTimeout(countdown);
+						return;
+					} else if (s <= 15) {
+						// Flash timer if less than 15 seconds left
+						ui.timer.style.color = (s % 2 === 0) ? '#FF3030' : 'transparent';
+					} else if (s <= 30) {
+						// Solid red timer when less than 30 seconds left.
+						ui.timer.style.color = '#FF3030';
+					}
+					ui.timer.innerHTML = m + ':' + visualS;
+				}, 1000);
+			} else {
+				s = 135;
+			}
+			NetworkTables.setValue(key, false);
+			break;
 	}
 }
